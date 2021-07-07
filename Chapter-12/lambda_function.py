57588e1119a8bf7e568edd6acbf8fa42f5f56fed
Adam Book <adam.book@cloudreach.com>
2021-07-07 01:15:28 -0400

    Chapter 12 files

--- Chapter-12/lambda_function.py
+++ Chapter-12/lambda_function.py
@@ -8,17 +8,17 @@ def lambda_handler(event, context):
 # get the URL from the event 
   r = requests.get("https://aws.amazon.com/blogs/compute/using-lambda-layers-to-simplify-your-development-process/") #demo 
   #r = requests.get(event['url'])
-  bs = BeautifulSoup(r.content)
+  bs = BeautifulSoup(r.content, "html.parser")
 
   # gather all the words within the paragraphs 
   p_txt = (''.join(s.findAll(text=True))for s in bs.findAll('p'))
   count_p = Counter((x.rstrip(punctuation).lower() for y in p_txt for x in y.split()))
 
   # gather all the text in the divs 
-  d_txt = (''.join(s.findAll(text=True))for s in soup.findAll('div'))
+  d_txt = (''.join(s.findAll(text=True))for s in bs.findAll('div'))
   count_div = Counter((x.rstrip(punctuation).lower() for y in d_txt for x in y.split()))
 
   # create a sum total of the words 
-  word_sum = p_txt + d_txt
+  word_sum = countp + count_div
   # return the number of words 
   return word_sum
